91541da12e7e85acb89c5dbc38ec2c3063c7d136
Asad Ahmed <asad@sheba.xyz>
2022-09-14 21:37:29 +0600

    Send Attendance Report Email
    Type(s): UPDATE
    
    Issue(s):
    - ClickUp: 2unh7m2

---
+++ resources/views/emails/custom-attendance-report.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{$title}}</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">
+            <p>Dear {{$employee_name}},</p>
+            <p>Please find your custom attendance report from {{$start_date}} to {{$end_date}} attached</p>
+            <p>Regards<br>sBusiness.xyz</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
