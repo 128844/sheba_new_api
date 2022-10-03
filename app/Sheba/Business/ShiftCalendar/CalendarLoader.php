716d02c49bcc490d11f56bfab5093dffe779e007
Shafiqul Islam <shafiqul@sheba.xyz>
2022-10-03 21:43:21 +0600

    🐛  Fix shift type filter
    Type(s): FIX
    
    Issue(s):
    - ClickUp: 306y4zj

--- app/Sheba/Business/ShiftCalendar/CalendarLoader.php
+++ app/Sheba/Business/ShiftCalendar/CalendarLoader.php
@@ -27,28 +27,46 @@ class CalendarLoader
 
         $period = CarbonPeriod::create($start_date, $end_date);
 
+        $business_member_ids = $this->getBusinessMembersWhoHasShiftType($business, $request, $period);
+
         $shift_calender_data = collect([]);
         foreach ($period as $date) {
-            $shift_calender_data->push($this->getQuery($business, $request, $date)->offset($offset)->take($limit)->get());
+            $data = $this->getQuery($business, $request, $business_member_ids)
+                ->where('date', $date->toDateString())
+                ->offset($offset)
+                ->take($limit)
+                ->get();
+            $shift_calender_data->push($data);
         }
 
         $shift_calender_data= $shift_calender_data->flatten(1);
 
-        $total_employees = $this->getQuery($business, $request, $start_date)->count(\DB::raw('DISTINCT business_member_id'));
+        $total_employees = $this->getQuery($business, $request)
+            ->where('date', $start_date->toDateString())
+            ->count(\DB::raw('DISTINCT business_member_id'));
 
         return [$shift_calender_data, $total_employees];
     }
 
-    private function getQuery(Business $business, Request $request, Carbon $date)
+    private function getBusinessMembersWhoHasShiftType(Business $business, Request $request, CarbonPeriod $period)
+    {
+        if (!$request->has('shift_type')) return null;
+
+        return $this->getQuery($business, $request)
+            ->whereBetween('date', [$period->getStartDate()->toDateString(), $period->getEndDate()->toDateString()])
+            ->where($request->shift_type, 1)
+            ->groupBy('business_member_id')
+            ->pluck('business_member_id')
+            ->toArray();
+    }
+
+    private function getQuery(Business $business, Request $request, $business_member_ids = null)
     {
         $shift_calender = $this->repo->builder()
             ->with('businessMember.member.profile', 'businessMember.role.businessDepartment')
-            ->where('date', $date->toDateString())
             ->whereHas('businessMember', function ($q) use ($business) {
                 $q->where('status', Statuses::ACTIVE)->where('business_id', $business->id);
             });
-
-        if ($request->has('shift_type')) $shift_calender = $shift_calender->where($request->shift_type, 1);
 
         if ($request->has('department_id')) {
             $shift_calender->whereHas('businessMember', function ($q) use ($request) {
@@ -66,6 +84,10 @@ class CalendarLoader
                     $q->where('name', 'LIKE', "%$request->search%");
                 })->orWhere('employee_id', 'LIKE', "%$request->search%");
             });
+        }
+
+        if (!empty($business_member_ids)) {
+            $shift_calender->whereIn('business_member_id', $business_member_ids);
         }
 
         return $shift_calender;
