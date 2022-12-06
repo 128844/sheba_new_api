de1782ee0506b3b13f0108edd50550501f0a41fa
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2022-12-06 14:51:48 +0600

    ACCOUNTING JOURNAL REPORT PAGINATION ADDED
    Type(s): UPDATE

--- app/Http/Controllers/Accounting/ReportsController.php
+++ app/Http/Controllers/Accounting/ReportsController.php
@@ -2,6 +2,7 @@
 
 
 use App\Http\Controllers\Controller;
+use App\Sheba\AccountingEntry\Constants\AccountingReport;
 use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\JsonResponse;
@@ -101,27 +102,41 @@ class ReportsController extends Controller
      */
     public function getAccountingReport(Request $request, $reportType): JsonResponse
     {
+        list($offset, $limit) = calculatePagination($request);
+
         if ($reportType == "details_ledger_report") {
             $this->validate($request, ['account_id' => 'required']);
         }
 
         $report_types = ["profit_loss_report", "journal_report", "balance_sheet_report", "general_ledger_report", "details_ledger_report", "general_accounting_report"];
-        $startDate = $request->start_date ? $request->start_date . ' 0:00:00' : Carbon::now()->format('Y-m-d') . ' 0:00:00';
-        $endDate = $request->end_date ? $request->end_date . ' 23:59:59' : Carbon::now()->format('Y-m-d') . ' 23:59:59';
-        $acc_id = (int)$request->account_id;
 
-        if ($endDate < $startDate) {
+        $start_date = $request->start_date ? $request->start_date . ' 0:00:00' : Carbon::now()->format('Y-m-d') . ' 0:00:00';
+        $end_date = $request->end_date ? $request->end_date . ' 23:59:59' : Carbon::now()->format('Y-m-d') . ' 23:59:59';
+
+        $account_id = (int)$request->account_id;
+
+        if ($end_date < $start_date) {
             return api_response($request, null, 400, ['message' => 'End date can not smaller than start date']);
         }
 
         if (in_array($reportType, $report_types)) {
-            $response = $this->accountingReportRepository->getAccountingReport($reportType, $request->partner->id, $startDate, $endDate, $acc_id, $request->account_type);
+            $response = $this->accountingReportRepository->getAccountingReport($reportType, $request->partner->id, $start_date, $end_date, $account_id, $request->account_type);
+
+            if ($reportType == AccountingReport::JOURNAL_REPORT) {
+                $response = array_slice($response, $offset, $limit);
+            }
+
             (new Usage())->setUser($request->partner)->setType(Usage::Partner()::REPORT_DOWNLOAD)->create($request->auth_user);
             return api_response($request, $response, 200, ['data' => $response]);
         }
+
         return api_response($request, null, 402, ['message' => 'Please apply with correct report type.']);
     }
 
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
     public function getAccountingReportsList(Request $request): JsonResponse
     {
         $version_code = $request->header('version-code');
@@ -129,15 +144,31 @@ class ReportsController extends Controller
         return api_response($request, $response, 200, ['data' => $response]);
     }
 
+    /**
+     * @param  Request  $request
+     * @return JsonResponse
+     */
     public function getTransactionList(Request $request): JsonResponse
     {
         $partner = $request->auth_user->getPartner();
-        $data = $this->accountingReportRepository->setLimit($request->limit)->setOffset($request->offset)->setStartDate($request->start_date)->setEndDate($request->end_date)
-            ->setTransactionType($request->transaction_type)->setReconcile($request->reconcile)->setGateway($request->gateway)->setQ($request->q)
+        $data = $this->accountingReportRepository
+            ->setLimit($request->limit)
+            ->setOffset($request->offset)
+            ->setStartDate($request->start_date)
+            ->setEndDate($request->end_date)
+            ->setTransactionType($request->transaction_type)
+            ->setReconcile($request->reconcile)
+            ->setGateway($request->gateway)->setQ($request->q)
             ->transactionList($partner->id);
+
         return api_response($request, $data, 200, ['data' => $data]);
     }
 
+    /**
+     * @param  Request  $request
+     * @param $id
+     * @return JsonResponse
+     */
     public function getTranactionDetails(Request $request, $id)
     {
         $partner = $request->auth_user->getPartner();
