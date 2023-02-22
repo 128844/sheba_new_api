c660862836615f04fbcc71c25e8f1f6d5b051543
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2023-02-22 22:43:49 +0600

    ➕ Be added aamarpay onboarding data
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://app.clickup.com/t/865brxg43

---
+++ app/Sheba/AamarpayOnboarding/AamarpayAccountStatus.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Sheba\AamarpayOnboarding;
+
+use App\Models\Partner;
+use App\Sheba\MTB\AuthTypes;
+use App\Sheba\MTB\MtbServerClient;
+use App\Sheba\QRPayment\QRPaymentStatics;
+
+class AamarpayAccountStatus
+{
+    /**
+     * @var Partner
+     */
+    private $partner;
+
+    public function __construct(MtbServerClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function setPartner(Partner $partner): MtbAccountStatus
+    {
+        $this->partner = $partner;
+        return $this;
+    }
+
+    public function checkAccountStatus()
+    {
+        $response = $this->client->get(QRPaymentStatics::MTB_ACCOUNT_STATUS.$this->partner->partnerMefInformation->mtb_ticket_id, AuthTypes::BARER_TOKEN);
+        $this->partner->partnerMefInformation->mtb_account_status = json_encode($response);
+        return $this->partner->partnerMefInformation->save();
+    }
+}
---
+++ app/Sheba/AamarpayOnboarding/AamarpaySaveNomineeInformation.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Sheba\AamarpayOnboarding;
+
+use App\Models\Partner;
+use App\Sheba\DynamicForm\PartnerMefInformation;
+use App\Sheba\MTB\AuthTypes;
+use App\Sheba\MTB\Exceptions\MtbServiceServerError;
+use App\Sheba\MTB\MtbConstants;
+use App\Sheba\MTB\MtbServerClient;
+use App\Sheba\QRPayment\QRPaymentStatics;
+use Sheba\TPProxy\TPProxyServerError;
+
+class AamarpaySaveNomineeInformation
+{
+    /**
+     * @var MtbServerClient
+     */
+    private $client;
+    /**
+     * @var Partner
+     */
+    private $partner;
+    /**
+     * @var PartnerMefInformation
+     */
+    private $partnerMefInformation;
+
+    public function __construct(MtbServerClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function setPartner(Partner $partner)
+    {
+        $this->partner = $partner;
+        return $this;
+    }
+
+    public function setPartnerMefInformation($partnerMefInformation): MtbSaveNomineeInformation
+    {
+        $this->partnerMefInformation = $partnerMefInformation;
+        return $this;
+    }
+
+    private function makeData(): array
+    {
+        return [
+            'RequestData' => [
+                'ticketId'          => $this->partner->partnerMefInformation->mtb_ticket_id,
+                'nomNm'             => json_decode($this->partnerMefInformation->partner_information)->nomineeName,
+                'nomFatherNm'       => json_decode($this->partnerMefInformation->partner_information)->nomineeFatherName,
+                'nomMotherNm'       => json_decode($this->partnerMefInformation->partner_information)->nomineeMotherName,
+                'nomDob'            => date("Ymd", strtotime(json_decode($this->partnerMefInformation->partner_information)->nomineeDOB)),
+                'nomMobileNum'      => json_decode($this->partnerMefInformation->partner_information)->nomineePhone,
+                'nomRelation'       => json_decode($this->partnerMefInformation->partner_information)->nomineeRelation,
+                'nid'               => json_decode($this->partnerMefInformation->partner_information)->nomineeNid,
+                'presentPostcode'   => json_decode($this->partnerMefInformation->partner_information)->nomineePresentPostCode,
+                'presentAddress'    => json_decode($this->partnerMefInformation->partner_information)->nomineePresentAddress,
+                'permanentPostCode' => json_decode($this->partnerMefInformation->partner_information)->nomineePermanentPostCode,
+                'permanentAddress'  => json_decode($this->partnerMefInformation->partner_information)->nomineePermanentAddress
+            ],
+            'requestId'   => strval($this->partner->id),
+            'channelId'   => MtbConstants::CHANNEL_ID
+        ];
+    }
+
+    private function checkIfBanglaInputExist($data)
+    {
+        $data = collect($data);
+        $flattened = $data->flatten()->toArray();
+        foreach ($flattened as $x => $flat) {
+            $isEnglish = $this->is_english($flat);
+            if (!$isEnglish) {
+                throw new MtbServiceServerError("অনুগ্রহ পূর্বক সব তথ্য ইংরেজিতে লিখুন");
+            }
+        }
+    }
+
+    function is_english($str)
+    {
+        if (strlen($str) != strlen(utf8_decode($str))) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @throws TPProxyServerError
+     * @throws MtbServiceServerError
+     */
+    public function storeNomineeInformation()
+    {
+        $data = $this->makeData();
+        $this->checkIfBanglaInputExist($data);
+        return $this->client->post(QRPaymentStatics::MTB_SAVE_NOMINEE_INFORMATION, $data, AuthTypes::BARER_TOKEN);
+    }
+
+}
