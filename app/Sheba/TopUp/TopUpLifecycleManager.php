e8f0bff9ad23b1747f082a579308657443e56e24
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2023-03-16 22:08:31 +0600

    🐛  Be deduct amount on ipn hit when agent not debited
    Type(s): FIX
    
    Issue(s):
    - JIRA: https://app.clickup.com/t/861mh1jqm

--- app/Sheba/TopUp/TopUpLifecycleManager.php
+++ app/Sheba/TopUp/TopUpLifecycleManager.php
@@ -1,9 +1,11 @@
-<?php namespace Sheba\TopUp;
+<?php
 
+namespace Sheba\TopUp;
 
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Redis;
 use Sheba\Dal\TopupOrder\FailedReason;
+use Sheba\Dal\TopupOrder\TopUpOrderRepository;
 use Sheba\Reward\ActionRewardDispatcher;
 use Sheba\TopUp\Gateway\HasIpn;
 use Sheba\TopUp\Vendor\Response\Ipn\FailResponse;
@@ -15,33 +17,48 @@ use Throwable;
 class TopUpLifecycleManager extends TopUpManager
 {
     /**
-     * @param FailResponse $fail_response
+     * @param  FailResponse  $fail_response
      * @throws Throwable
      */
     public function fail(FailResponse $fail_response)
     {
-        if ($this->topUpOrder->isFailed()) return;
+        if ($this->topUpOrder->isFailed()) {
+            return;
+        }
 
         $this->doTransaction(function () use ($fail_response) {
             $this->statusChanger->failed(FailDetails::buildFromIpnFailResponse($fail_response));
-            if ($this->topUpOrder->isAgentDebited()) $this->refund();
+            if ($this->topUpOrder->isAgentDebited()) {
+                $this->refund();
+            }
             $this->getVendor()->refill($this->topUpOrder->amount);
         });
         // $this->sendPushNotification("দুঃখিত", "দুঃখিত, কারিগরি ত্রুটির কারনে " .$this->topUpOrder->payee_mobile. " নাম্বারে আপনার টপ-আপ রিচার্জ সফল হয়নি। অনুগ্রহ করে আবার চেষ্টা করুন।");
     }
 
     /**
-     * @param SuccessResponse $success_response
+     * @param  SuccessResponse  $success_response
      * @throws Throwable
      */
     public function success(SuccessResponse $success_response)
     {
-        if ($this->topUpOrder->isSuccess()) return;
+        if ($this->topUpOrder->isSuccess()) {
+            return;
+        }
 
-        $this->doTransaction(function () use ($success_response) {
+        $order_repo = app(TopUpOrderRepository::class);
+        $vendor = $this->getVendor();
+
+        $this->doTransaction(function () use ($success_response, $order_repo, $vendor) {
             $details = $success_response->getTransactionDetails();
             $id = $success_response->getUpdatedTransactionId();
             $this->topUpOrder = $this->statusChanger->successful($details, $id);
+
+            if (!$this->topUpOrder->isAgentDebited()) {
+                $this->topUpOrder->agent->getCommission()->setTopUpOrder($this->topUpOrder)->disburse();
+                $vendor->deductAmount($this->topUpOrder->amount);
+                $order_repo->update($this->topUpOrder, ['is_agent_debited' => 1]);
+            }
         });
 
         if ($this->topUpOrder->isSuccess()) {
@@ -56,7 +73,9 @@ class TopUpLifecycleManager extends TopUpManager
      */
     public function reload()
     {
-        if (!$this->topUpOrder->canRefresh()) return;
+        if (!$this->topUpOrder->canRefresh()) {
+            return;
+        }
         $vendor = $this->getVendor();
         $response = $vendor->enquire($this->topUpOrder);
         $response->handleTopUp();
@@ -76,8 +95,8 @@ class TopUpLifecycleManager extends TopUpManager
 
     private function logIpn(IpnResponse $ipn_response, $request_data)
     {
-        $key = 'Topup::' . ($ipn_response instanceof FailResponse ? "Failed:failed" : "Success:success") . "_";
-        $key .= Carbon::now()->timestamp . '_' . $ipn_response->getTopUpOrder()->id;
+        $key = 'Topup::'.($ipn_response instanceof FailResponse ? "Failed:failed" : "Success:success")."_";
+        $key .= Carbon::now()->timestamp.'_'.$ipn_response->getTopUpOrder()->id;
         Redis::set($key, json_encode($request_data));
     }
 }
