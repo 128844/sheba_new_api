c34ec806d4b5398a7d4ce2f4f75c39259fa99394
Asad Ahmed <asadrabbi@polygontech.xyz>
2023-08-01 20:22:55 +0600

    Enable Sheba Employee Remote Attendance
    Type(s): ADD

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -381,7 +381,7 @@ class Business extends BaseModel implements TopUpAgent, PayableUser, HasWalletTr
 
     private function isForceRemoteAttendanceEnable($business_member_id): bool
     {
-        $sheba_employees = [573, 574, 674, 2041, 6895, 13529, 16460, 16461, 16462];
+        $sheba_employees = [573, 574, 674, 2041, 6895, 13529, 16460, 16461, 16462, 19393, 19614];
         return in_array($business_member_id, $sheba_employees);
     }
 
